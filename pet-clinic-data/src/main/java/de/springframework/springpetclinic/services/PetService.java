4fca2f61c574096f493db1b7b1dbc8025a142a79
Britta Lange <britta.lange@mailbox.org>
2021-07-10 16:25:14 +0200

    Adding interfaces for services. Closes #4

---
+++ pet-clinic-data/src/main/java/de/springframework/springpetclinic/services/PetService.java
@@ -0,0 +1,13 @@
+package de.springframework.springpetclinic.services;
+
+import de.springframework.springpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
