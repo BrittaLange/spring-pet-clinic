07eef4960e4137a3ff864d8108cddfbd0a6969be
Britta Lange <britta.lange@mailbox.org>
2021-07-10 16:47:52 +0200

    Adding base entity. Closes #9.

--- pet-clinic-data/src/main/java/de/springframework/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/de/springframework/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package de.springframework.springpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/de/springframework/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/de/springframework/springpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package de.springframework.springpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
     private PetType petType;
     private Owner owner;
     private LocalDate birthday;
