ae05a3cd1d72d8fdd2480d8c51df72cfcec905d9
Britta Lange <britta.lange@mailbox.org>
2021-08-28 17:21:45 +0200

    Added SpecialityService and SpecialityServiceMap Impl. Closes #30.

---
+++ pet-clinic-data/src/main/java/de/springframework/springpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package de.springframework.springpetclinic.services;
+
+import de.springframework.springpetclinic.model.Specialty;
+
+public interface SpecialityService extends CrudService<Specialty, Long>{
+}
